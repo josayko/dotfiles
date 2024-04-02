0ffb3fb5d749920a78bf0e4f3d1dbb21fe5a1db5
Jonny Saykosy <josayko@pm.me>
2024-04-02 23:16:12 +0200

    update color iex

--- .iex.exs
+++ .iex.exs
@@ -1,25 +1,36 @@
 # IEx.configure colors: [enabled: true]
 # IEx.configure colors: [ eval_result: [ :cyan, :bright ] ]
-IO.puts IO.ANSI.magenta_background() <> IO.ANSI.white() <> " ❄❄❄ All things serve the BEAM ❄❄❄ " <> IO.ANSI.reset
+IO.puts(
+  IO.ANSI.color_background(2, 1, 3) <>
+    IO.ANSI.white() <> " ❄❄❄ All things serve the BEAM ❄❄❄ " <> IO.ANSI.reset()
+)
+
 Application.put_env(:elixir, :ansi_enabled, true)
+
 IEx.configure(
   colors: [
-    eval_result: [:green, :bright] ,
-    eval_error: [[:red,:bright,"Bug Bug ..!!"]],
-    eval_info: [:yellow, :bright ],
+    eval_result: [:green, :bright],
+    eval_error: [[:red, :bright, "Bug Bug ..!!"]],
+    eval_info: [:yellow, :bright]
   ],
-  default_prompt: [
-    "%prefix",:white,"|",
+  default_prompt:
+    [
+      "%prefix",
+      :white,
+      "|",
       :blue,
       "%counter",
       :white,
       "|",
-    :red,
-    "▶" ,         # plain string
-    :white,
-    "▶▶"  ,       # plain string
+      :red,
+      # plain string
+      "▶",
+      :white,
+      # plain string
+      "▶▶",
       # ❤ ❤-»" ,  # plain string
-    :reset
-  ] |> IO.ANSI.format() |> IO.chardata_to_string
+      :reset
+    ]
+    |> IO.ANSI.format()
+    |> IO.chardata_to_string()
 )
-
